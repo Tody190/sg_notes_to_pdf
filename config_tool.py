76102d79b62294fb6e61fa1c3e42e7217dab34fa
yangtao <toddyyang1990@foxmail.com>
2019-09-02 13:40:35 +0800

    添加 config 模块

---
+++ config_tool.py
@@ -0,0 +1,74 @@
+# -*- coding: utf-8 -*-
+__author__ = "yangtao"
+
+
+import os
+import configparser
+
+
+
+
+config_path = os.path.dirname(__file__).replace('\\', '/') + '/config.ini'
+config_dict = {"SG": {"url":"",
+                      "script_name":"",
+                      "api_key":""}}
+
+config = configparser.ConfigParser()
+
+
+def generate_config(config_dict):
+    # 创建 config 文件
+    for sect in config_dict:
+        config[sect] = config_dict[sect]
+    with open(config_path, 'w') as configfile:
+        config.write(configfile)
+
+
+def read(section=None, value=None):
+    if not os.path.exists(config_path):
+        generate_config(config_dict)
+
+    config.read(config_path)
+    if section and not value:
+        value_dict = {}
+        for v in config[section]:
+            value_dict[v] = config[section][v]
+        return value_dict
+    elif section and value:
+        return config[section][value]
+    elif not section and value:
+        raise ValueError("section is %s"%section)
+    else:
+        result = {}
+        for sect in config:
+            result[sect] = {}
+            for v in config[sect]:
+                result[sect][v] = config[sect][v]
+        return result
+
+
+def write(section, value):
+    if not section:
+        raise ValueError("section is %s" % section)
+
+    config_dict = read()
+    if section not in config_dict:
+        config_dict[section] = {}
+
+    if value:
+        value_map = config_dict[section]
+        for v in value:
+            value_map[v.lower()] = value[v]
+
+        config_dict[section] = value_map
+
+    generate_config(config_dict)
+
+
+
+
+if __name__ == "__main__":
+    value = {"URL": "https://dezerlin.shotgunstudio.com",
+             "SCRIPT_NAME": "sg_notes_to_HTML",
+             "API_KEY": "wxbxjmmh?pEhumhyosynir6ve"}
+    write("SG", value)
---
+++ dezerlin_welcome.py
@@ -0,0 +1,17 @@
+# coding=utf-8
+__author__ = 'yangtao'
+__version__ = '0.1'
+
+
+dezerlin_show = " _______   _______  ________   _______ .______       __       __  .__   __. \n"
+dezerlin_show += "|       \ |   ____||       /  |   ____||   _  \     |  |     |  | |  \ |  | \n"
+dezerlin_show += "|  .--.  ||  |__   `---/  /   |  |__   |  |_)  |    |  |     |  | |   \|  | \n"
+dezerlin_show += "|  |  |  ||   __|     /  /    |   __|  |      /     |  |     |  | |  . `  | \n"
+dezerlin_show += "|  '--'  ||  |____   /  /----.|  |____ |  |\  \----.|  `----.|  | |  |\   | \n"
+dezerlin_show += "|_______/ |_______| /________||_______|| _| `._____||_______||__| |__| \__|"
+
+def show(author, version):
+    print(dezerlin_show)
+    print("author: %s   version: %s"%(author, version))
+    print("enjoy it!")
+    print('')
