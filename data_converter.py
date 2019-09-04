aebeb6e4c932d12fcca7ee92dbe1e27b3ea90846
yangtao <toddyyang1990@foxmail.com>
2019-09-04 17:46:05 +0800

    逻辑修改

---
+++ data_converter.py
@@ -0,0 +1,115 @@
+# -*- coding: utf-8 -*-
+__author__ = "yangtao"
+
+
+import codecs
+import os
+import subprocess
+import json
+
+
+
+
+class Html_Notes:
+    def __html(self, body):
+        html = "<!DOCTYPE html>\n"
+        html += "<html lang=\"zh-cn\">\n"
+        html += self.__head()
+        html += body
+        html += "</html>"
+        return html
+
+    def __head(self):
+        head = "<head>\n"
+        head += "<meta http-equiv=\"Content-Type\" content=\"text/html; charset=UTF-8\">\n"
+        head += self.__style()
+        head += "</head>\n"
+        return head
+
+    def __style(self):
+        style = "<style type=\"text/css\">\n"
+        style += "h1 {color:red}\n"
+        style += "h2 {color:gray}\n"
+        style += "p {font-weight: 700}\n"
+        style += "</style>\n"
+        return style
+
+    def __body(self, version, subject, content, attachments):
+        body = "\n"
+        # version
+        body += "<h1>\n"
+        body += "%s\n"%version
+        body += "</h1>\n"
+        # subject
+        body += "<h2>\n"
+        body += "%s\n"%subject
+        body += "</h2>\n"
+        # content
+        body += "<p>\n"
+        body += "%s\n"%content
+        body += "</p>\n"
+        # attachments
+        for attachment in attachments:
+            body += "<a href=\"%s\" target=\"_blank\">\n" % attachment
+            body += "<img src=\"%s\" width=\"480\" height=\"270\"/>\n" % attachment
+            body += "</a>\n"
+        body += "<br />\n"
+        body += "<br />\n"
+
+        return body
+
+    def build(self, notes_data):
+        # add body
+        body = ""
+        for version_name in notes_data:
+            subject = notes_data[version_name]["subject"]
+            content = notes_data[version_name]["content"]
+            attachments = notes_data[version_name]["attachments"]
+            body += self.__body(version_name, subject, content, attachments)
+
+        return self.__html(body)
+
+
+def name_filter(playlist_name):
+    # 将不可以命名的字符改为空格
+    special_symbol = ["\\", "/", ":", "*", "?", "\"", "<", ">", "|"]
+    new_name = " "
+    for s in playlist_name:
+        if s in special_symbol:
+            s = " "
+        new_name += s
+    return new_name.lstrip()
+
+
+def write_html(html_file, info):
+    html_path = os.path.dirname(html_file)
+    if not os.path.exists(html_path):
+        os.makedirs(html_path)
+    with codecs.open(html_file, 'w', 'utf-8') as f:
+        f.write(info)
+
+
+def notes_to_html(notes_data, output_path, file_name):
+    hn = Html_Notes()
+    html_info = hn.build(notes_data)
+    # html 文件路径
+    html_file = os.path.join(output_path, "%s.html" % file_name)
+    write_html(html_file, html_info)
+    if os.path.exists(html_file):
+        return html_file
+
+
+def htm_to_pdf(html_file, pdf_file):
+    wkhtmltopdf_path = os.path.dirname(__file__).replace('\\', '/') + '/wkhtmltox/bin/wkhtmltopdf.exe'
+    if not os.path.exists(wkhtmltopdf_path):
+        raise ("%s 不存在"%wkhtmltopdf_path)
+    p = subprocess.Popen([wkhtmltopdf_path, html_file, pdf_file])
+    p.communicate()
+
+
+
+
+if __name__ == "__main__":
+    from collections import OrderedDict
+    notes_data = OrderedDict([('h45010.cfx.sim.v003', OrderedDict([('subject', 'choki-cfx.sim-QC-20190827 - August 27th'), ('content', '动态ok，进预渲染'), ('attachments', [])])), ('h45130.cfx.sim.v003', OrderedDict([('subject', 'choki-cfx.sim-QC-20190827 - August 27th'), ('content', 'fox 夹克内T恤应该是紧身，不要松垮'), ('attachments', ['D:\\temp\\html\\choki-cfx.sim-QC-20190827\\test\\attachments\\annot_version_18841.188.png', 'D:\\temp\\html\\choki-cfx.sim-QC-20190827\\test\\attachments\\annot_version_18841.280.png'])])), ('h95470.cfx.sim.v002', OrderedDict([('subject', 'choki-cfx.sim-QC-20190827 - August 29th'), ('content', '抱歉！我必须补充一点，这个镜头是表现飞船超快速下落，头发和衣服都必须表现出惯性的晃动，等同于有风在45度向画面右上方吹。'), ('attachments', [])])), ('h95530.cfx.sim.v001', OrderedDict([('subject', 'choki-cfx.sim-QC-20190827 - August 29th'), ('content', '头发请加个持续晃动！'), ('attachments', [])])), ('i20150.cfx.sim.v004', OrderedDict([('subject', "ella's Note on i20150.cfx.sim.v004 and i20150"), ('content', '拍屏文件多了一帧'), ('attachments', [])])), ('i20190.cfx.sim.v001', OrderedDict([('subject', 'choki-cfx.sim-QC-20190827 - August 27th'), ('content', '布料错误太多'), ('attachments', ['D:\\temp\\html\\choki-cfx.sim-QC-20190827\\test\\attachments\\annot_version_18846.101.png', 'D:\\temp\\html\\choki-cfx.sim-QC-20190827\\test\\attachments\\annot_version_18846.36.png', 'D:\\temp\\html\\choki-cfx.sim-QC-20190827\\test\\attachments\\annot_version_18846.94.png'])])), ('i20250.cfx.sim.v003', OrderedDict([('subject', "ella's Note on i20250.cfx.sim.v003 and i20250"), ('content', '拍屏文件多了3帧'), ('attachments', [])])), ('i20270.cfx.sim.v004', OrderedDict([('subject', 'choki-cfx.sim-QC-20190827 - August 27th'), ('content', '夹克太飘，其他ok'), ('attachments', ['D:\\temp\\html\\choki-cfx.sim-QC-20190827\\test\\attachments\\annot_version_18849.110.png', 'D:\\temp\\html\\choki-cfx.sim-QC-20190827\\test\\attachments\\annot_version_18849.79.png'])])), ('i20390.cfx.sim.v002', OrderedDict([('subject', "ella's Note on i20390.cfx.sim.v002 and i20390"), ('content', '拍屏文件多了6帧'), ('attachments', [])])), ('i20410.cfx.sim.v002', OrderedDict([('subject', 'choki-cfx.sim-QC-20190827 - August 29th'), ('content', 'Dylan头发垂度有点错误'), ('attachments', [])]))])
+    notes_to_html(notes_data)
--- sg_data.py
+++ sg_data.py
@@ -3,22 +3,32 @@ __author__ = "yangtao"
 
 
 import collections
+import os
 
 import shotgun_api3
 
 try:
     import sg_script
+    url = sg_script.URL
+    script_name = sg_script.SCRIPT_NAME
+    api_key = sg_script.API_KEY
 except:
-    raise Exception("需要一个叫 sg_script.py 配置文件, 文件内包含 URL，SCRIPT_NAME，API_KEY 三个全局变量")
+    import config_tool
+    url = config_tool.read("SG", "url")
+    script_name = config_tool.read("SG", "script_name")
+    api_key = config_tool.read("SG", "api_key")
+    if not url or not script_name or not  api_key:
+        raise Exception("config.ini 需要填写一些必要信息")
 
 
+
+
+SG = shotgun_api3.Shotgun(url,
+                          script_name=script_name,
+                          api_key=api_key)
 
 
 class Playlist_Notes:
-    sg = shotgun_api3.Shotgun(sg_script.URL,
-                              script_name=sg_script.SCRIPT_NAME,
-                              api_key=sg_script.API_KEY)
-
     def __init__(self, playlist_name: str):
         self.playlist_name = playlist_name
 
@@ -35,25 +45,65 @@ class Playlist_Notes:
             for n in note_entitys:
                 notes[v["name"]]["subject"] = n["subject"]
                 notes[v["name"]]["content"] = n["content"]
+                notes[v["name"]]["attachments"] = n["attachments"]
         return notes
 
     def get_playlist_versions(self, playlist_name) -> list:
         # 获取所有版本文件
         fields = ["versions"]
         filters = [["code", "is", playlist_name]]
-        playlists_entity = self.sg.find("Playlist", filters, fields)
+        playlists_entity = SG.find("Playlist", filters, fields)
         if len(playlists_entity) == 1:
             return playlists_entity[0]["versions"]
+        elif len(playlists_entity) == 0:
+            raise Exception("找不到名为：\"%s\" 的 Playlist" % playlist_name)
         else:
             raise Exception("多于一个 Playlist 实体名字为：%s"%playlist_name)
 
     def get_version_notes(self, version_entity):
-        fields = ["subject", "content", "user", "addressings_to"]
+        fields = ["id", "subject", "content", "user", "addressings_to", "attachments"]
         filters = [["note_links", "in", version_entity]]
-        note_entitys = self.sg.find("Note", filters, fields)
+        note_entitys = SG.find("Note", filters, fields)
         return note_entitys
+
+
+def download_attachment(attachment_entity, download_path):
+    if not os.path.exists(download_path):
+        os.makedirs(download_path)
+
+    attachment_file = os.path.join(download_path, attachment_entity["name"])
+    SG.download_attachment(attachment_entity, attachment_file)
+    #print("Download: %s"%attachment_file)
+
+    if os.path.exists(attachment_file):
+        return attachment_file
+
+
+def get_notes_data(playlist, attachments_download_path=None):
+    pn = Playlist_Notes(playlist)
+    notes_data = pn.get()
+
+    for v in notes_data.values():
+        attachments = v["attachments"]
+        new_attachments = []
+        # 下载附件并将保存地址替换原值
+        for attachment in attachments:
+            if attachments_download_path:
+                attachment_url = download_attachment(attachment, attachments_download_path)
+            else:
+                attachment_url = SG.get_attachment_download_url(attachment)
+
+            if attachment_url:
+                new_attachments.append(attachment_url)
+
+        v["attachments"] = new_attachments
+
+    return notes_data
+
+
+
 
 if __name__ == "__main__":
     import pprint
-    pn = Playlist_Notes("choki-cfx.sim-QC-20190827")
-    print(pn.get())
+    notes_data = get_notes_data("choki-cfx.sim-QC-20190827", r"D:\temp\html\choki-cfx.sim-QC-20190827\test")
+    pprint.pprint(notes_data)
